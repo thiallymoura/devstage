1f4ee2ae9864311bfc99ed9e3d0ddea1b4ee7526
Thially Moura <thiallymoura@hotmail.com>
2025-02-19 11:27:39 -0300

    creating interface, components and form

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,10 +1,14 @@
 import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-interface ButtonProps extends ComponentProps<'button'> {}
-export function IconButton(props: ButtonProps) {
+interface IconButtonProps extends ComponentProps<'button'> {}
+export function IconButton({ className, ...props }: IconButtonProps) {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900',
+        className
+      )}
       {...props}
     />
   )
